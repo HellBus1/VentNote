c5f26f5f6e0103e57b13a4b27c967d4a8a06452e
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2023-03-17 15:12:06 +0700

    feat: init note creation page

--- app/src/main/java/com/digiventure/ventnote/feature/noteCreation/NoteCreationPage.kt
+++ app/src/main/java/com/digiventure/ventnote/feature/noteCreation/NoteCreationPage.kt
@@ -1,9 +1,22 @@
 package com.digiventure.ventnote.feature.noteCreation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.digiventure.ventnote.feature.noteCreation.components.NoteCreationAppBar
 
 @Composable
 fun NoteCreationPage(navHostController: NavHostController) {
+    Scaffold(
+        topBar = {
+            NoteCreationAppBar()
+        },
+    ) { contentPadding ->
+        Box(modifier = Modifier.padding(contentPadding)) {
 
+        }
+    }
 }
---
+++ app/src/main/java/com/digiventure/ventnote/feature/noteCreation/components/AppBar.kt
@@ -0,0 +1,8 @@
+package com.digiventure.ventnote.feature.noteCreation.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NoteCreationAppBar() {
+
+}
